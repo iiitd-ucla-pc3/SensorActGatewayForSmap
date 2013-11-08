3889470e9f222683b4a0d9d1fc7f062e5e7d88fc
samy <pandarasamya@iiitd.ac.in>
2013-11-07 22:39:01 -0800

    DD

--- app/controllers/Downloader.java
+++ app/controllers/Downloader.java
@@ -25,9 +25,12 @@ public class Downloader extends Job {
 		return time.toLocaleString() + "\nCounter : " + counter + "\n\n" + data; 
 	}
 
-	public void doJob() {
+	public void doJob() {		
+		System.out.println("..inside doJob");
+		isRunning = true;
 		while (isRunning) {			
 			try {
+				System.out.println("requestion...");
 				HttpResponse res = WS.url("http://192.168.1.40:9101/republish")
 						.get();
 				InputStream is = res.getStream();
@@ -35,7 +38,8 @@ public class Downloader extends Job {
 				DataInputStream dis = new DataInputStream(is);
 				String line = null;
 
-				while ((line = dis.readLine()) != null) {					
+				while ((line = dis.readLine()) != null) {
+					System.out.println(line);
 					data = line;
 					counter++;
 					time = new Date();
